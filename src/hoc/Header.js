485ab356dcce3e29fd39251df992eada5a4cb8f0
vitalik-fan <vital.pereg@mail.ru>
2022-06-06 10:54:16 +0300

    все закоммитили

--- src/hoc/Header.js
+++ src/hoc/Header.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import logo from './../assets/Logo.svg';
+import { Link } from "react-router-dom";
+import hamburger from './../assets/Hamburger.svg';
 
 const Header = (props) => {
   return (
     <>
       <header className="header">
-        Header
+        <Link className="header__logo" to="/">
+          <img  src={logo} alt="Logo"/>
+        </Link>
+        <div className="header__controllers">
+          123
+        </div>
+        <div className="header__info">
+          <span className="header__info-who">
+            Yura Karmakov
+          </span>
+          <span>
+            <img src={hamburger} alt="hamburger"/>
+          </span>
+        </div>
       </header>
       {props.children}
     </>
