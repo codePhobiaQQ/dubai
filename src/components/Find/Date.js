f280b5fd72ddfcc0330e2bab22f11b07c74ba78a
vitalik-fan <vital.pereg@mail.ru>
2022-06-08 14:13:51 +0300

    высота

--- src/components/Find/Date.js
+++ src/components/Find/Date.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import del from "../../assets/delete.svg";
 import DataRangePicker from "../DataRangePicker";
 
 const Date = (props) => {
+  const popup = useRef(null)
+
   const dateHandler = () => {
     if (props.whatOpen != 2) {
       props.setWhatOpen(2)
     }
   }
+
+  useEffect(() => {
+    const windowHeight = document.querySelector('.calendar-content').clientHeight
+    popup.current.setAttribute("style", `height: ${windowHeight}px;`)
+  }, [])
 
   return (
     <div onClick={dateHandler} className="findEl__input">
@@ -20,7 +27,7 @@ const Date = (props) => {
       <div className={props.whatOpen == 2 ? "findEl__input-delete active" : "findEl__input-delete"}>
         <img src={del} alt="del"/>
       </div>
-      <div className={props.whatOpen == 2 ? "datePopup whenPopup active popup" : "datePopup whenPopup popup"}>
+      <div ref={popup} className={props.whatOpen == 2 ? "datePopup whenPopup active popup" : "datePopup whenPopup popup"}>
         <DataRangePicker />
       </div>
     </div>
