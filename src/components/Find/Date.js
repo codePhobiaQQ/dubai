05c45904d11adcbd64b4bf1263be6a94d4b05e31
vitalik-fan <vital.pereg@mail.ru>
2022-06-08 14:19:05 +0300

    вроде зафиксил

--- src/components/Find/Date.js
+++ src/components/Find/Date.js
@@ -16,7 +16,7 @@ const Date = (props) => {
   useEffect(() => {
     const windowHeight = document.querySelector('.calendar-content').clientHeight
     popup.current.setAttribute("style", `height: ${windowHeight}px;`)
-  }, [width, height])
+  }, [width, height, props.whatOpen])
 
   return (
     <div onClick={dateHandler} className="findEl__input">
