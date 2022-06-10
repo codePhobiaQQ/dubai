c67a145a3580d4a73f56acbcc60071a545a18a55
vitalik-fan <vital.pereg@mail.ru>
2022-06-10 18:02:56 +0300

    mode on blur

--- src/components/UI/CustomInput.js
+++ src/components/UI/CustomInput.js
@@ -1,23 +1,42 @@
 import React, {useState} from 'react';
+import { MaskedInput, createDefaultMaskGenerator, DEFAULT_MASK_RULES  } from 'react-hook-mask';
 
-const CustomInput = ({className, placeholder, name, errors, register}) => {
+const maskGenerator = {
+  rules: DEFAULT_MASK_RULES,
+  generateMask: (value) =>
+    (value?.replaceAll('-', '').length ?? 0) <= 13
+      ? '9-(999)-999-99-99'
+      : '9-(999)-999-999-999',
+  transform: (v) => v?.toUpperCase(),
+};
+
+const CustomInput = ({className, type = "string", placeholder, name, errors, register, getValues}) => {
   const [isFocus, setIsFocus] = useState(false)
 
-  console.log(register(name).ref)
+  const onBlurHandler = () => {
+    if (!getValues(name)) setIsFocus(false)
+  }
 
   return (
-    <div
-      onFocus={() => setIsFocus(true)}
-      onBlur={() => setIsFocus(false)}
-      className={isFocus ? "active " + className : className}
-    >
-      <span className={"labelInput"}>{placeholder}</span>
-      <input
-        name={name}
-        {...register(name)}
-      />
-      {errors[name]?.message}
-    </div>
+      <div
+        onFocus={() => setIsFocus(true)}
+        onBlur={onBlurHandler}
+        className={isFocus ? "active " + className : className}
+      >
+        <span className={errors[name] ? "labelInput error" : "labelInput"}>{placeholder}</span>
+        {!(name == "Phone")
+          ? <input
+              name={name}
+              type={type}
+              {...register(name)}
+            />
+          : <MaskedInput
+            maskGenerator={maskGenerator}
+            {...register(name)}
+          />
+        }
+        <span className="InputError">{errors[name] ? errors[name]?.message : ""}</span>
+      </div>
   );
 };
 
--- src/pages/ConfirmPay.js
+++ src/pages/ConfirmPay.js
@@ -6,17 +6,17 @@ import * as yup from "yup";
 import CustomInput from "../components/UI/CustomInput";
 
 const schema = yup.object({
-  FirstName: yup.string().required(),
-  LastName: yup.string().required(),
+  FirstName: yup.string().required("First Name can`t be empty"),
+  LastName: yup.string().required("Last Name can`t be empty"),
+  Email: yup.string().email("Please enter the valid e-mail"),
 }).required();
 
 const ConfirmPay = () => {
-  const { register, handleSubmit, watch, formState: { errors } } = useForm({
-    resolver: yupResolver(schema)
+  const { register, handleSubmit, formState: { errors }, getValues } = useForm({
+    resolver: yupResolver(schema),
+    mode: "onBlur",
   });
   const onSubmit = data => console.log(data);
-
-  console.log(watch("example")); // watch input value by passing the name of it
 
   return (
     <Header>
@@ -24,22 +24,22 @@ const ConfirmPay = () => {
          <div className="container container_flex">
             <div className="container__inner">
               <div className="inner-left-container">
-                <CustomInput errors={errors} className={"inputMix"} placeholder={"First Name"} name={"FirstName"} register={register} />
+
                 <form onSubmit={handleSubmit(onSubmit)}>
                   <h2>Confirm and pay</h2>
-
-                  <input className={"inputMix"} placeholder={"First name"} {...register("FirstName")} />
-                  <input className="inputMix" placeholder={"Last name"} {...register("LastName")} />
-                  <input className="inputMix" placeholder={"E-mail"} {...register("Email")} />
-                  <input className="inputMix" placeholder={"Phone number"} {...register("PhoneNumber")} />
-                  <input className="inputMix" placeholder={"Check-in time"} {...register("CheckInTime")} />
-                  <input className="inputMix" placeholder={"Comment"} {...register("Comment")} />
+                  <CustomInput getValues={getValues} errors={errors} className={"inputMix"} placeholder={"First Name"} name={"FirstName"} register={register} />
+                  <CustomInput getValues={getValues} errors={errors} className={"inputMix"} placeholder={"E-mail"} name={"Email"} register={register} />
+                  <CustomInput getValues={getValues} errors={errors} className={"inputMix"} placeholder={"Phone number"} name={"Phone"} register={register} />
+                  <CustomInput getValues={getValues} errors={errors} className={"inputMix"} placeholder={"Check-in time"} name={"CheckInTime"} register={register} />
+                  <CustomInput getValues={getValues} errors={errors} className={"inputMix"} placeholder={"Comment"} name={"Comment"} register={register} />
                   {/*{errors.exampleRequired && <span>This field is required</span>}*/}
 
                   <h2 className="secondTitle">Pay with</h2>
-                  <input className="inputMix" placeholder={"Expiration"} {...register("Expiration")} />
-                  <input className="inputMix" placeholder={"CVV"} {...register("CVV")} />
+                  <CustomInput getValues={getValues} errors={errors} className={"inputMix"} placeholder={"Expiration"} name={"Expiration"} register={register} />
+                  <CustomInput getValues={getValues} errors={errors} className={"inputMix"} placeholder={"CVV"} name={"CVV"} register={register} type={"password"} />
                   {/*<input className="inputMix" type="submit" />*/}
+
+                  <button type={"submit"}>submit</button>
                 </form>
               </div>
               <div className="inner-right-container">
