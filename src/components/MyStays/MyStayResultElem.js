061d93f9c895f2a50265eb64a28ddb954b696589
vitalik-fan <vital.pereg@mail.ru>
2022-06-11 19:50:08 +0300

    left side done

--- src/components/MyStays/MyStayResultElem.js
+++ src/components/MyStays/MyStayResultElem.js
@@ -1,12 +1,38 @@
 import React from 'react';
+import chair from "../../assets/chair.svg"
+import more from "../../assets/more.svg"
 
-const MyStayResultElem = ({image}) => {
+const MyStayResultElem = ({image, name, rooms, size, daying, guests, time}) => {
   return (
     <div className="MyStayResultElem">
       <div className="MyStayResultElemLeft">
-        <img src={image} alt="apart"/>
+        <img className="bigImg" src={image} alt="apart"/>
         <div className="MyStayResultElemLeftContent">
-          left
+          <h3>{name}</h3>
+          <div className="detailsEls">
+            <div className="detailEl">
+              <img src={chair} alt="chair"/>
+              <span>{rooms} rooms</span>
+            </div>
+            <div className="detailEl">
+              <img src={more} alt="more"/>
+              <span>{size} m2</span>
+            </div>
+          </div>
+          <div className="extraInfo">
+            <div className="extraInfoLine">
+              <span>Dates</span>
+              <span>{daying}</span>
+            </div>
+            <div className="extraInfoLine">
+              <span>Guests</span>
+              <span>{guests}</span>
+            </div>
+            <div className="extraInfoLine">
+              <span>Check-in time</span>
+              <span>{time}</span>
+            </div>
+          </div>
         </div>
       </div>
       <div className="MyStayResultElemRight">
--- src/pages/MyStayResult.js
+++ src/pages/MyStayResult.js
@@ -10,8 +10,24 @@ const MyStayResult = () => {
       <section className="myStays">
         <div className="container">
           <h2>My stays</h2>
-          <MyStayResultElem image={apart1} />
-          <MyStayResultElem image={apart2} />
+          <MyStayResultElem
+            name={"Deluxe Marina Flat Marina Flat Marina "}
+            rooms={2}
+            image={apart1}
+            size={170}
+            daying={"26 May - 4 June (6 nights)"}
+            guests={2}
+            time={"11:00 - 12:00"}
+          />
+          <MyStayResultElem
+            name={"Deluxe Marina Flat Marina"}
+            rooms={2}
+            image={apart2}
+            size={170}
+            daying={"26 May - 4 June (6 nights)"}
+            guests={2}
+            time={"11:00 - 12:00"}
+          />
         </div>
       </section>
     </Header>
