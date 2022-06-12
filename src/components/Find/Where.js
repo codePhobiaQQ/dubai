257116cf30510973808a9e613e427f47ce970b8e
vitalik-fan <vital.pereg@mail.ru>
2022-06-12 17:51:58 +0300

    done popup

--- src/components/Find/Where.js
+++ src/components/Find/Where.js
@@ -10,41 +10,64 @@ const Where = (props) => {
     }
   }
 
+  const guestsHandler = () => {
+    if (props.whatOpen != 1) {
+      props.setWhatOpen(1)
+    }
+  }
+
   return (
-    <div className="findEl__input">
-      <div className={props.whatOpen == 1 ? "findEl__where-label active" : "findEl__where-label"}>
-        Where are you going?
-      </div>
-      <input onClick={whereHandler} onChange={whereHandler} className={props.whatOpen == 1 ? "active" : ""} placeholder="Where are you going?" />
-      <div className={props.whatOpen == 1 ? "findEl__input-delete active" : "findEl__input-delete"}>
-        <img src={del} alt="del"/>
+
+    <div onClick={guestsHandler} className="findEl__input">
+      <div className="findEl__input-label timeLabel">
+        <span>Where are you going?</span>
+        <div className={props.whatOpen == 1 ? "findEl__guest-amount active" : "findEl__guest-amount"}>
+          Dubai
+        </div>
       </div>
       <div className={props.whatOpen == 1 ? "wherePopup active popup" : "wherePopup popup"}>
         <ul>
           <li>
             Dubai
           </li>
-          {/*<li>*/}
-          {/*  Jumeirah Burj Al Arab*/}
-          {/*</li>*/}
-          {/*<li>*/}
-          {/*  Dubai Burj Al Arab*/}
-          {/*</li>*/}
-          {/*<li>*/}
-          {/*  Dubai Burj Al Arab*/}
-          {/*</li>*/}
-          {/*<li>*/}
-          {/*  Dubai Burj Al Arab*/}
-          {/*</li>*/}
-          {/*<li>*/}
-          {/*  Dubai Burj Al Arab*/}
-          {/*</li>*/}
-          {/*<li>*/}
-          {/*  Dubai Burj Al Arab*/}
-          {/*</li>*/}
         </ul>
       </div>
     </div>
+
+    // <div className="findEl__input">
+    //   <div className={props.whatOpen == 1 ? "findEl__where-label active" : "findEl__where-label"}>
+    //     Where are you going?
+    //   </div>
+    //   <input onClick={whereHandler} onChange={whereHandler} className={props.whatOpen == 1 ? "active" : ""} placeholder="Where are you going?" />
+    //   <div className={props.whatOpen == 1 ? "findEl__input-delete active" : "findEl__input-delete"}>
+    //     <img src={del} alt="del"/>
+    //   </div>
+    //   <div className={props.whatOpen == 1 ? "wherePopup active popup" : "wherePopup popup"}>
+    //     <ul>
+    //       <li>
+    //         Dubai
+    //       </li>
+    //       <li>
+    //         Jumeirah Burj Al Arab
+    //       </li>
+    //       <li>
+    //         Dubai Burj Al Arab
+    //       </li>
+    //       <li>
+    //         Dubai Burj Al Arab
+    //       </li>
+    //       <li>
+    //         Dubai Burj Al Arab
+    //       </li>
+    //       <li>
+    //         Dubai Burj Al Arab
+    //       </li>
+    //       <li>
+    //         Dubai Burj Al Arab
+    //       </li>
+    //     </ul>
+    //   </div>
+    // </div>
   );
 };
 
